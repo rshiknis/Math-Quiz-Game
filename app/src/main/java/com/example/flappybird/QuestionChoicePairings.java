a199e57053c352679709e810e5b555d7c8391332
Nirjhar Deb <ndeb6@gatech.edu>
2024-09-06 19:51:31 -0400

    Added a basic UI and question-answer combos

---
+++ app/src/main/java/com/example/flappybird/QuestionChoicePairings.java
@@ -0,0 +1,21 @@
+package com.example.flappybird;
+
+public class QuestionChoicePairings {
+    public static String questionChoices[] ={
+            "Which of these animals is a reptile?",
+            "Which of these numbers is prime?",
+            "Which of these numbers is odd?"
+    };
+
+    public static String answerChoices[][] = {
+            {"Chimpanzee","Crocodile","Monkey"},
+            {"17","69","420"},
+            {"29","2","4"},
+    };
+
+    public static String correctAnswerChoices[] = {
+            "Crocodile",
+            "17",
+            "29"
+    };
+}
