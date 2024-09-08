16ea2b88b1f07966dfda3323888006fbaa051300
Nirjhar Deb <ndeb6@gatech.edu>
2024-09-08 01:09:06 -0400

    In login page, added functionality to signin and signup button. They can transition to the appropriate page, but login "verification logic" is still needed.

--- app/src/main/java/com/example/flappybird/Login.java
+++ app/src/main/java/com/example/flappybird/Login.java
@@ -2,13 +2,38 @@ package com.example.flappybird;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button loginButton, signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signupButton = findViewById(R.id.signup_button);
+        loginButton = findViewById(R.id.login_button);
+
+        // TODO: ADD LOGIN LOGIC TO VERIFY USER/PASSWORD COMBO
+        // Upon click of login button, transition to the login page
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, MainActivity.class));
+            }
+        });
+
+        // Upon click of signup button, transition to the registration page
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
     }
 }
